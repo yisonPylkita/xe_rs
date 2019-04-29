c000bd05c3c8e457d73a9c415ea9d84ebc3e2ef8
Wojciech Bartnik <yisonPylkita@gmail.com>
2019-04-29 11:44:07 +0200

    Hex value will be printed as hex

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::fs;
 
+fn print_hex_data(content: &Vec<u8>) {
+    // Print only 16 bytes in a row
+    for chunk in content.chunks(16) {
+        for byte in chunk {
+            print!("{:02x} ", byte);
+        }
+        println!("");
+    }
+}
+
 fn main()
 {
-    let content = fs::read("Cargo.toml");
-    println!("{:?}", content);
+    let content = fs::read("Cargo.toml").unwrap();
+    print_hex_data(&content);
 }
