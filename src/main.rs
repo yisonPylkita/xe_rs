916cd11f04d2740f863b59a28ac7f19abb1e6b62
Wojciech Bartnik <yisonPylkita@gmail.com>
2019-04-29 16:01:02 +0200

    Print with visible ASCII chars

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,26 @@ use std::fs;
 fn print_hex_data(content: &Vec<u8>) {
     // Print only 16 bytes in a row
     for chunk in content.chunks(16) {
+        // print hex view
         for byte in chunk {
             print!("{:02x} ", byte);
         }
+        print!("    ");
+        // print string view
+        for byte in chunk {
+            match byte {
+                0x00 ... 0x1f => {
+                    print!(".");
+                },
+                0x20 ... 0x7e => {
+                    print!("{}", *byte as char);
+                },
+                0x7f ... 0xff => {
+                    print!(".");
+                },
+            }
+        }
+
         println!("");
     }
 }
